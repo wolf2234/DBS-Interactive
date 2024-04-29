2b036cdd8bf56a7f3bcba83ba11c900c7eab5484
wolf2234 <viktoral9599@gmail.com>
2024-04-29 17:24:57 +0300

    this commit adds styles for adaptation.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@
 
 const hamMenu = document.querySelector(".hamburger");
 const offScreenMenu = document.querySelector(".menu");
+const darkside = document.querySelector(".wrapper");
+const dropdown = document.querySelector(".dropdown");
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
-    offScreenMenu.classList.toggle('active')
+    offScreenMenu.classList.toggle('active');
+    darkside.classList.toggle('darkside');
+    dropdown.classList.toggle('active');
 });
