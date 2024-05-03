d81dd577ec860f8fb4ef53b3b30537fe4c25dff6
wolf2234 <viktoral9599@gmail.com>
2024-05-03 12:47:09 +0300

    this commit adds a delta button and dropdown block.

--- js/script.js
+++ js/script.js
@@ -2,16 +2,19 @@
 
 const hamMenu = document.querySelector(".burger");
 const offScreenMenu = document.querySelector(".menu");
-// const body = document.getElementsByTagName('body');
 const body = document.querySelector('.body');
-// const darkside = document.querySelector(".wrapper");
+// const delta = document.querySelector('.menu__delta');
 // const dropdown = document.querySelector(".dropdown");
 
 hamMenu.addEventListener('click', () => {
     hamMenu.classList.toggle('active');
     offScreenMenu.classList.toggle('active');
     body.classList.toggle('lock');
-    // darkside.classList.toggle('darkside');
-    // dropdown.classList.toggle('active');
-
 });
+
+// delta.addEventListener('click', () => {
+//     dropdown.style.display = "block"
+// });
+// delta.addEventListener('blur', () => {
+//     dropdown.style.display = "none"
+// });
